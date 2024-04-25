659bb46f8962dbd5658879a16c534b4399341c26
G-Prasath <guruprasathmsc@gmail.com>
2024-04-25 16:20:29 +0530

    Testing for port

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,6 @@ const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3001; // Assuming your React app runs on port 3000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -22,53 +21,58 @@ app.use((req, res, next) => {
 // POST route to handle form submission
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server Is Running Now`);
-  app.post("/send-email", (req, res) => {
-    const { username, email, phone, service } = req.body;
-    res.send("Hello World");
+  console.log(`Server Is Running Now ${PORT}`);
 
-    const transporter = nodemailer.createTransport({
-      service: "Gmail", // Update with your email service provider
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const htmlContent = `
-    <h2>${process.env.EMAIL_HEADING}</h2>
-    <table border="1" cellpadding="5" cellspacing="0">
-      <tr>
-        <td><strong>Name:</strong></td>
-        <td>${username}</td>
-      </tr>
-      <tr>
-        <td><strong>Email:</strong></td>
-        <td>${email}</td>
-      </tr>
-      <tr>
-        <td><strong>Phone:</strong></td>
-        <td>${phone}</td>
-      </tr>
-      <tr>
-        <td><strong>Service:</strong></td>
-        <td>${service}</td>
-      </tr>
-    </table>
-  `;
-
-    const mailOptions = {
-      from: email,
-      to: process.env.EMAIL_USER,
-      subject: "PEB Enquiry Quote",
-      html: htmlContent,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send("Error sending email");
-      }
-    });
+  app.post("/test", (req, res) => {
+    res.send("Response from /test endpoint");
   });
+
+  // app.post("/send-email", (req, res) => {
+  //   const { username, email, phone, service } = req.body;
+  //   res.send("Hello World");
+
+  //   const transporter = nodemailer.createTransport({
+  //     service: "Gmail", // Update with your email service provider
+  //     auth: {
+  //       user: process.env.EMAIL_USER,
+  //       pass: process.env.EMAIL_PASS,
+  //     },
+  //   });
+
+  //   const htmlContent = `
+  //   <h2>${process.env.EMAIL_HEADING}</h2>
+  //   <table border="1" cellpadding="5" cellspacing="0">
+  //     <tr>
+  //       <td><strong>Name:</strong></td>
+  //       <td>${username}</td>
+  //     </tr>
+  //     <tr>
+  //       <td><strong>Email:</strong></td>
+  //       <td>${email}</td>
+  //     </tr>
+  //     <tr>
+  //       <td><strong>Phone:</strong></td>
+  //       <td>${phone}</td>
+  //     </tr>
+  //     <tr>
+  //       <td><strong>Service:</strong></td>
+  //       <td>${service}</td>
+  //     </tr>
+  //   </table>
+  // `;
+
+  //   const mailOptions = {
+  //     from: email,
+  //     to: process.env.EMAIL_USER,
+  //     subject: "PEB Enquiry Quote",
+  //     html: htmlContent,
+  //   };
+
+  //   transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //       console.error(error);
+  //       res.status(500).send("Error sending email");
+  //     }
+  //   });
+  // });
 });
