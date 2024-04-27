dc83a714196ab9de0b8006c16c5dfa1ccbaeb2fc
G-Prasath <82494750+G-Prasath@users.noreply.github.com>
2024-04-27 15:52:03 +0530

    from add

--- Server/index.js
+++ Server/index.js
@@ -69,7 +69,7 @@ app.listen(process.env.PORT, () => {
   `;
 
     const mailOptions = {
-      from: email,
+      from: username,
       to: process.env.EMAIL_USER,
       subject: "PEB Enquiry Quote",
       html: htmlContent,
