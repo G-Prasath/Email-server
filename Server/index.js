8f566015d91f43f0174e18b6edb613b1de0cb35d
G-Prasath <82494750+G-Prasath@users.noreply.github.com>
2024-05-25 14:24:45 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -24,9 +24,10 @@ app.listen(process.env.PORT, () => {
   console.log(`Server Is Running Now ${process.env.PORT}`);
 
   app.post("/send-email", (req, res) => {
-    const { username, email, phone, service, toAddr, mailHeading } = req.body;
+    const { username, email, phone, service } = req.body;
     res.send("Hello World");
-    res.send(req.body);
+    console.log("Inside send-email");
+
     const transporter = nodemailer.createTransport({
       service: "Gmail", // Update with your email service provider
       auth: {
@@ -36,7 +37,7 @@ app.listen(process.env.PORT, () => {
     });
 
     const htmlContent = `
-    <h2>${mailmailHeading} Request for this Client</h2>
+    <h2>Request for this Client</h2>
     <table border="1" cellpadding="5" cellspacing="0">
       <tr>
         <td><strong>Name:</strong></td>
@@ -59,8 +60,8 @@ app.listen(process.env.PORT, () => {
 
     const mailOptions = {
       from: "estarinfra.com",
-      to: toAddr,
-      subject: `${mailHeading} Enquiry Quote`,
+      to: process.env.EMAIL_USER,
+      subject: "Enquiry Quote",
       html: htmlContent,
     };
 
